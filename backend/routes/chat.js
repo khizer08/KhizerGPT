218a09d25f56017f57123e68521997d51ae23092
khizer08 <syedkhizer2004@gmail.com>
2026-04-23 15:44:40 +0530

    ready-to-deploy

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Thread from "../models/thread.js";
+import Thread from "./models/thread.js";
 import getGeminiAPIResponse from "../utils/gemini.js";
 
 const router = express.Router();
